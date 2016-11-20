1526e8d67a4c40e7741afe25c0f9fb719dbe56ba
davidfastgreene <dgreen88@uncc.edu>
2016-11-20 10:27:09 -0500

    Added Graph with data, Added SeekBar, Gave custom values to RadioButtons and Seekbar

--- app/src/main/java/com/example/david/eande/MainActivity.java
+++ app/src/main/java/com/example/david/eande/MainActivity.java
@@ -1,37 +1,94 @@
 package com.example.david.eande;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.RadioButton;
+import android.widget.SeekBar;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.highlight.Highlight;
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 import static android.widget.Toast.LENGTH_SHORT;
 
 public class MainActivity extends AppCompatActivity {
 
-    public Double timeRemaining = 0.0;
+    //Variables
+    public Integer timeRemaining = 8;
     final Integer time = 24;
     public Integer work = 8;
-    public Integer sleep = 7;
+    public Integer sleep = 0;
     public Integer activities = 0;
 
+    //Global Interfaces
+    private PieChart pieChart;
+    SeekBar sb_sleepHours;
+
+    //Data
+    private float[] yData = {};
+    private String[] xData = {"W", "S", "R"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        activities = time - (work + sleep);
+        pieChart = (PieChart)findViewById(R.id.pieChart);
+        sb_sleepHours =(SeekBar) findViewById(R.id.sb_sleepHours);
 
-        RadioButton rbdefault = (RadioButton)findViewById(R.id.rb_default);
+        pieChart.getLegend().setEnabled(false);
+        pieChart.setRotationEnabled(false);
+        pieChart.setUsePercentValues(true);
+        pieChart.setCenterTextColor(Color.BLACK);
+        pieChart.setHoleRadius(35f);
+        pieChart.setTransparentCircleAlpha(0);
+        pieChart.setCenterText("Daily Time");
+        pieChart.setCenterTextSize(18);
+        pieChart.setDrawEntryLabels(true);
+        pieChart.setEntryLabelTextSize(20);
+
+        //Call method for PieChart creation
+        addDataSet();
+
+        pieChart.setOnChartValueSelectedListener(new OnChartValueSelectedListener() {
+            @Override
+            public void onValueSelected(Entry e, Highlight h) {
+
+                int pos1 = e.toString().indexOf("(sum): ");
+                String employee = xData[pos1 + 1];
+
+                if (e == null) {return;}
+
+                Toast.makeText(MainActivity.this, employee,
+                        Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected() {}
+        });
+
+        //Radio Button options
+        final RadioButton rbdefault = (RadioButton)findViewById(R.id.rb_default);
         rbdefault.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                activities = time - (work + sleep);
-                Toast toast = Toast.makeText(getApplicationContext(), ("default" + activities), LENGTH_SHORT);
-                toast.show();
+                timeRemaining = 8;
+                Toast.makeText(getApplicationContext(), ("You work: " + timeRemaining), LENGTH_SHORT).show();
+
             }
         });
 
@@ -39,9 +96,8 @@ public class MainActivity extends AppCompatActivity {
         rbswing.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                activities = time - (work + 5);
-                Toast toast = Toast.makeText(getApplicationContext(), ("swing" + activities), LENGTH_SHORT);
-                toast.show();
+                timeRemaining = 8;
+                Toast.makeText(getApplicationContext(), ("swing" + activities), LENGTH_SHORT).show();
             }
         });
 
@@ -49,12 +105,74 @@ public class MainActivity extends AppCompatActivity {
         rbgraveyard.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                activities = time - (work + 2);
-                Toast toast = Toast.makeText(getApplicationContext(), ("graveyard" + activities), LENGTH_SHORT);
-                toast.show();
+                timeRemaining = 8;
+                Toast.makeText(getApplicationContext(), ("graveyard" + activities), LENGTH_SHORT).show();
             }
         });
-        
+
+        //Spinner sleep hour options
+        sb_sleepHours.setMax(12);
+        sb_sleepHours.setProgress(6);
+        sb_sleepHours.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener(){
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress,
+                                          boolean fromUser) {
+                //sleep = Integer.valueOf(progress);
+                Toast.makeText(getApplicationContext(), ("Sleep hours" + String.valueOf(progress)), LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                // TODO Auto-generated method stub
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                // TODO Auto-generated method stub
+            }
+        });
+    }
+
+
+    private void addDataSet() {
+        ArrayList<PieEntry> yEntrys = new ArrayList<>(); //colors
+        ArrayList<String> xEntrys = new ArrayList<>(); //
+        yEntrys.add(new PieEntry(timeRemaining, 0));
+        yEntrys.add(new PieEntry(timeRemaining, 1));
+        yEntrys.add(new PieEntry(timeRemaining, 2));
+
+//        //adds input to the array
+//        for(int i = 0; i < yData.length; i++){
+//            yEntrys.add(new PieEntry(yData[i] , i));
+//        }
+
+        for(int i = 1; i < xData.length; i++){
+            xEntrys.add(xData[i]);
+        }
+
+        //create the data set
+        PieDataSet pieDataSet = new PieDataSet(yEntrys, "");
+        pieDataSet.setSliceSpace(2);
+        pieDataSet.setValueTextSize(14);
+
+        //add colors to data set
+        ArrayList<Integer> colors = new ArrayList<>(); //colors array
+        colors.add(Color.parseColor("#00BFA5"));
+        colors.add(Color.parseColor("#0D47A1"));
+        colors.add(Color.parseColor("#E65100"));
+
+        pieDataSet.setColors(colors);
+
+        //add legend to chart
+        Legend legend = pieChart.getLegend();
+        legend.setForm(Legend.LegendForm.CIRCLE);
+        legend.setPosition(Legend.LegendPosition.LEFT_OF_CHART);
+
+        //create pie data object
+        PieData pieData = new PieData(pieDataSet);
+        pieChart.setData(pieData);
+        pieChart.invalidate();
     }
 
 
